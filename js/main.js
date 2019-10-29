f8623b9ff914a6c64cd6cf93b5772bc46d822e1b
Alexcor43 <alexcordev@gmail.com>
2019-10-29 10:11:50 -0400

    Pages completed- Commit before changing background layout

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*globals $:false 
+'use_strict'
+
+$(function(){
+  $("#head").load("head.html"); 
+  $("#header").load("header.html"); 
+  $("#footer").load("footer.html"); 
+  $("#head").load("/pages/head.html"); 
+  $("#header").load("/pages/header.html"); 
+  $("#footer").load("/pages/footer.html"); 
+  
+  });*/
+ 
---
+++ js/main1.js
@@ -0,0 +1,9 @@
+/*globals $:false 
+'use_strict'
+
+$(function(){
+  $("#head").load("pages/head.html"); 
+  $("#header").load("pages/header.html"); 
+  $("#footer").load("pages/footer.html"); 
+  
+  });*/
